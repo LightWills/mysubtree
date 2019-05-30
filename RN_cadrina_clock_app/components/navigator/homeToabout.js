f94182eb04e292ec8c5e34681418a7550936effb
Williams <williamsviyou97@gmail.com>
2019-05-30 23:05:44 +0200

    Two view with the good icon

--- RN_cadrina_clock_app/components/navigator/homeToabout.js
+++ RN_cadrina_clock_app/components/navigator/homeToabout.js
@@ -1,11 +1,86 @@
 import  { createBottomTabNavigator, createAppContainer } from 'react-navigation';
+// import Ionicons from 'react-native-vector-icons/Ionicons';
 import About from '../about/About';
 import Home from '../home/Home';
+import { Ionicons } from '@expo/vector-icons'; // 6.2.2
+import React from 'react';
+import { Platform, StatusBar, StyleSheet, View, Text } from 'react-native';
+import MyConfig from '../../myConfig/styles_color';
 
+/**
+ * This class is using for switnig in a view to another view
+ */
+const config = new MyConfig();
+class IconWithBadge extends React.Component {
+    render() {
+      const { name, badgeCount, color, size } = this.props;
+      return (
+        <View style={{ width: 24, height: 24, margin: 5 }}>
+          <Ionicons name={name} size={size} color={color} />
+          {badgeCount > 0 && (
+            <View
+              style={{
+                // /If you're using react-native < 0.57 overflow outside of the parent
+                // will not work on Android, see https://git.io/fhLJ8
+                position: 'absolute',
+                right: -6,
+                top: -3,
+                backgroundColor: 'red',
+                borderRadius: 6,
+                width: 12,
+                height: 12,
+                justifyContent: 'center',
+                alignItems: 'center'
+                
+              }}>
+              <Text style={{ color: 'white', fontSize: 10, fontWeight: 'bold' }}>
+                {badgeCount}
+              </Text>
+            </View>
+          )}
+        </View>
+      );
+    }
+  }
+  
+  const HomeIconWithBadge = props => {
+    // You should pass down the badgeCount in some other ways like context, redux, mobx or event emitters.
+    return <IconWithBadge {...props} badgeCount={3} />;
+  };
+const getTabBarIcon = (navigation, focused, tintColor) => {
+    const { routeName } = navigation.state;
+    let IconComponent = Ionicons;
+    let iconName;
+    if (routeName === 'Home') {
+      iconName = `ios-alarm`;
+      // We want to add badges to home tab icon
+      IconComponent = HomeIconWithBadge;
+    } else if (routeName === 'About') {
+        iconName = `ios-information-circle${focused ? '' : '-outline'}`;
+    }
+    
+    // You can return any component that you like here!
+    return <IconComponent name={iconName} size={25} color={tintColor} />;
+  };
 
-const MyTabNavigator = createBottomTabNavigator({
+  
+const MyTabNavigator = createBottomTabNavigator(
+    {
     Home: Home,
     About: About,
-  });
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, tintColor }) =>
+        getTabBarIcon(navigation, focused, tintColor),
+    }),
+    tabBarOptions: {
+      activeTintColor: "black" ,
+      inactiveTintColor: config.baseColorButtonInactive ,
+    },
+  }
+  );
+
+  
   export default createAppContainer(MyTabNavigator);
   
---
+++ RN_cadrina_clock_app/myConfig/styles_color.js
@@ -0,0 +1,22 @@
+
+
+
+/**
+|--------------------------------------------------
+| This class is using for keep all genaral config
+|--------------------------------------------------
+*/
+
+export default class MyConfig {
+    // const {baseColorButton} = "black";
+    // this.props.baseColorButton  = "black";
+    // const var baseColorButton = "black"
+    constructor() {
+    const  baseColorButtonActive = "black"
+    const  baseColorButtonInactive= "gray"
+
+         
+    }
+
+}
+
